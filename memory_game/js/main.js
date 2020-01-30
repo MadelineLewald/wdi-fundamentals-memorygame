f4c63d0a639fd7ac5789c8221d727647324acbca
Madeline Lewald <madelinelewald@Madelines-MacBook-Pro.local>
2020-01-30 00:47:28 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,26 @@
 console.log("Up and running!");
-let cardOne = "Queen";
-let cardTwo = "Queen";
-let cardThree = "King";
-let cardFour = "King";
-console.log("User flipped" + cardOne);
+
+
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+
+
+let cardOne = cards[0];
+let cardTwo = cards[2];
+let cardThree = cards[1];
+let cardFour = cards[3];
+
+console.log("User flipped" + cardFour);
 console.log("User flipped" + cardTwo);
+
+cardsInPlay.push(cardTwo);
+
+cardsInPlay.push(cardFour);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1])	{
+		alert("you found a match");  
+	} else {
+		alert("sorry, try again");
+	}
+}
